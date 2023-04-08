062f778d7ba0a4beef81aa229f23721d43c5d804
Horace Smith <horacesmith26@gmail.com>
2023-04-08 15:10:39 -0500

    Initial commit

---
+++ src/main/java/com/cryptex/cryptexspringtrader/CryptexSpringTraderApplication.java
@@ -0,0 +1,13 @@
+package com.cryptex.cryptexspringtrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptexSpringTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptexSpringTraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptex/cryptexspringtrader/CryptexSpringTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptex.cryptexspringtrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptexSpringTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
