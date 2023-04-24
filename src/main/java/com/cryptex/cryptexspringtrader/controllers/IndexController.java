2882cea98a4efeeebef2064824ab84a55101b3e9
Horace Smith <horacesmith26@gmail.com>
2023-04-24 08:46:01 -0500

    successful merge with stash

--- src/main/java/com/cryptex/cryptexspringtrader/controllers/IndexController.java
+++ src/main/java/com/cryptex/cryptexspringtrader/controllers/IndexController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
             return "index";
         }
 
-
     @RequestMapping("/dashboard")
     public String dashboard() {
         return "dashboard";
