bba344942061aa1a3a127661122f5f48bb46d3f4
Horace Smith <horacesmith26@gmail.com>
2023-04-27 14:42:54 -0500

    changed

--- src/main/java/com/cryptex/cryptexspringtrader/config/SecurityConfiguration.java
+++ src/main/java/com/cryptex/cryptexspringtrader/config/SecurityConfiguration.java
@@ -126,10 +126,10 @@ public class  SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf()
-//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//                .and()
-                .csrf().disable()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
